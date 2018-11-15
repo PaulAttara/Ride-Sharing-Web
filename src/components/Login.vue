ef82fe06da984b40565cea78c43fc38e716e809d
Noam Suissa <noam.suissa01@gmail.com>
2018-11-15 15:57:38 -0500

    login ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,30 @@
 <template>
+  <div id="login">
+    <ul>
+      <li><input type="text" v-model="username" placeholder="Username"></li>
+      <li><input type="password" v-model="password" placeholder="Password"></li>
+      <li><button @click="login(username, password)">Login</button></li>
+    </ul>
+  </div>
 </template>
 
-<script>
+<script src="./login.js">
 </script>
 
-<style>
+<style scoped>
+
+#login{
+  padding-top: 100px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
 </style>
---
+++ src/components/login.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'login',
+  data () {
+    return {
+      username: [],
+      password: '',
+      errorLogin: '',
+      response: []
+    }
+  },
+  created: function () {
+  },
+  methods: {
+    login: function (username, password) {
+      
+    }
+  }
+}
