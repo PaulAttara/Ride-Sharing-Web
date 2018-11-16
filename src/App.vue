5d1ed894d6171ef3fe4ffcc1ead386b4d756bafe
Noam Suissa <noam.suissa01@gmail.com>
2018-11-15 22:13:56 -0500

    changed colour scheme

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  background-image: linear-gradient(to bottom right, rgba(150, 16, 232, 0.87), rgb(226, 170, 27));
+  background-image: linear-gradient(to bottom right, rgba(16, 57, 3, 0.85), rgba(5, 247, 175, 0.84));
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
