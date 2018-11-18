ca704019dba5bb72332f151dc1aabc351a56bd1a
Noam Suissa <noam.suissa01@gmail.com>
2018-11-18 17:36:31 -0500

    destination implemented

--- src/jsFiles/overview.js
+++ src/jsFiles/overview.js
@@ -24,30 +24,37 @@ export default {
   data () {
     return {
       routes: [],
+      destination: [],
       errorRoute: '',
       response: []
     }
   },
-  created: function () {
-    AXIOS.get('/api/route/getAllRoutes/', {}, {})
-      .then(response => {
-        // JSON responses are automatically parsed.
-        this.response = response.data;
-        //console.log(this.response.length)
-        for (var i = 0; i < this.response.length; i++) {
-          var newDate = response.data[0].date.toString();
-          var route = new RouteDto(response.data[i].routeId, response.data[i].car.driver.username, response.data[i].seatsAvailable, response.data[i].startLocation, "", newDate.split('T')[0], "en route");
-          this.routes.push(route);
-        }
-      })
-      .catch(e => {
-        var errorMsg = e.message;
-        console.log(errorMsg);
-        this.errorRoute = errorMsg;
-      });
+  created: async function () {
+    try{
+    let response = await AXIOS.get('/api/route/getAllRoutes/', {}, {});
+    this.response = response.data;
+    for (var i = 0; i < this.response.length; i++) {
+      var newDate = response.data[0].date.toString();
+      var route = new RouteDto(response.data[i].routeId, response.data[i].car.driver.username, response.data[i].seatsAvailable, response.data[i].startLocation, "", newDate.split('T')[0], "en route");
+      this.routes.push(route);
+    }
+  }catch(error){
+      console.log(error.message);
+      this.errorRoute = error.message;
+    }
+
+    //new axios get
+    try{
+    for (var i = 0; i < this.routes.length; i++) {
+      let response = await AXIOS.get('/api/location/getDestination/'+this.routes[i].id+'/', {}, {});
+      this.destination = response.data;
+      this.routes[i].dest = this.destination.city.toString();
+    }
+  }catch(error){
+    console.log(error.message);
+    this.errorRoute = error.message;
+  }
   },
   methods: {
-    getRoutes: function () {
-    }
   }
 }
