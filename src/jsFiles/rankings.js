51739409b34b098e6b22de48890c2342f4fdf13c
Eden Ovadia <eden.ovadia@mail.mcgill.ca>
2018-11-20 12:55:53 -0500

    Added comments and fixed the format of the code

--- src/jsFiles/rankings.js
+++ src/jsFiles/rankings.js
@@ -91,17 +91,6 @@ export default {
     }
   },
   created: async function () {
-    // // Test participants
-    // const p1 = new ParticipantDto('John')
-    // const p2 = new ParticipantDto('Jill')
-    // const event = new EventDto('Gala', '2018-10-31', '18:00', '23:00')
-    // const event1 = new EventDto('Coachella', '2018-11-31', '17:00', '23:59')
-    // p2.events.push(event)
-    // p2.events.push(event1)
-    // // Test event for p2 participant
-    // // Sample initial content
-    // this.participants = [p1, p2]
-
 
     try{
     let response = await AXIOS.get('/api/route/getAllRoutes/', {}, {});
@@ -116,6 +105,7 @@ export default {
       this.errorRoute = error.message;
     }
 
+
     //new axios get
     try{
     for (var i = 0; i < this.routes.length; i++) {
@@ -128,12 +118,12 @@ export default {
     this.errorRoute = error.message;
     }
 
+
     for (var i = 0; i < this.routes.length; i++) {
       var unique = true;
       // go through the array so far and compare the new value to all existing values
-
-        // if the current start and end date exist in the common start and end arrays, incrememnt the counter at that position
-        // set unique to false so this pair doesnt get added to the arrays
+      // if the current start and end date exist in the common start and end arrays, incrememnt the counter at that position
+      // set unique to false so this pair doesnt get added to the arrays
 
         if(this.commonStart.length != 0){
           for (var j = 0; j < this.commonStart.length; j++) {
@@ -149,10 +139,12 @@ export default {
         }
         // if unique is set to true, this pair is unique. add them to the arrays and make the counter equal to 1
       if(unique){
-          this.commonStart.push(this.routes[i].start);
+
+        this.commonStart.push(this.routes[i].start);
         this.commonEnd.push(this.routes[i].dest);
         this.counter.push(1);
-      }
+
+        }
 
       }
       // print the routes start and end location in order based on the counter:
@@ -160,20 +152,26 @@ export default {
       // add the corresponding route from routes to new array to print
       //get the next max number...
       while (true) {
+
         var index = indexOfMax(this.counter); // the index is the place in the array with the most occurences
         if (this.counter[index]=== -1) {
           break;
         }
+
         this.sortedStart.push(this.commonStart[index]); // add the route start with the most occurences to the sorted list
         this.sortedEnd.push(this.commonEnd[index]); // add the route end with the most occurences to the sorted list
         this.sortedOccurences.push(this.counter[index]);// add the number of occurences to the sorted list
         this.counter[index]=-1; // the current max now becomes -1
 
       }
+
+      // make a new sorted  DTO to use to display the routes
       for (var x = 0; x < this.sortedStart.length; x++) {
         this.sRoutes.push(new SortedRouteDto(this.sortedOccurences[x], this.sortedStart[x], this.sortedEnd[x]));
       }
+
       // DRIVER IN ORDER BY RANK
+      // do the same think for passenger in the next section
       try{
       let response = await AXIOS.get('/api/user/getAllUsers/driver', {}, {});
       this.response = response.data;
@@ -181,36 +179,46 @@ export default {
         var driver = new DriverDto(response.data[i].avgRating, response.data[i].username, response.data[i].numTrips);
         this.drivers.push(driver);
       }
+
       }catch(error){
         console.log(error.message);
         this.errorRoute = error.message;
       }
+
+
       // sort the drivers by avgrating
       // put in new sortedDriverDto
-
       var ratings = [];
-      for (var i = 0; i < this.drivers.length; i++){
+      for (var i = 0; i < this.drivers.length; i++){// put all the avgRatings in a new ratings array
         ratings.push(this.drivers[i].avgRating);
       }
+
+      // set up the arrays that will store the driver data
       var sortedUsername = [];
       var sortedAvgrating =[];
       var sortedNumTrips=[];
+
+
         while (true) {
         var dIndex= indexOfMax(ratings)// get index of highest rating
-        if (ratings[dIndex]=== -1) {
-          break;
-        }
-        sortedUsername.push(this.drivers[dIndex].username);// add the username at the index of the highest rating
-        sortedAvgrating.push(this.drivers[dIndex].avgRating);
-        sortedNumTrips.push(this.drivers[dIndex].numTrips);
-        ratings[dIndex]=-1;// current max now becomes -1
-      }
+          if (ratings[dIndex]=== -1) {
+            break;
+          }
 
+        sortedUsername.push(this.drivers[dIndex].username);// add the username at the index of the highest rating
+        sortedAvgrating.push(this.drivers[dIndex].avgRating);// add the avg rating at the proper index
+        sortedNumTrips.push(this.drivers[dIndex].numTrips);// add the num of trips at the proper index
+        ratings[dIndex]=-1;// current max now becomes -1
+
+      }
+      // make a new sorted Driver DTO to display all the sorted driver information
         for (var x = 0; x < sortedUsername.length; x++) {
           this.sDrivers.push(new SortedDriverDto(sortedAvgrating[x], sortedUsername[x], sortedNumTrips[x]));
         }
 
+
         // PASSENGER IN ORDER BY RANK
+        // same thing as driver
         try{
         let response = await AXIOS.get('/api/user/getAllUsers/passenger', {}, {});
         this.response = response.data;
@@ -222,33 +230,40 @@ export default {
           console.log(error.message);
           this.errorRoute = error.message;
         }
-        // sort the drivers by avgrating
-        // put in new sortedDriverDto
 
+
+        // sort the passengers by avgrating
+        // put in new sortedPassengerDto
         var pRatings = [];
         for (var i = 0; i < this.passengers.length; i++){
           pRatings.push(this.passengers[i].avgRating);
         }
+
+        // set up passenger arrays used for sorting
         var pSortedUsername = [];
         var pSortedAvgrating =[];
         var pSortedNumTrips=[];
+
           while (true) {
           var pIndex= indexOfMax(pRatings)// get index of highest rating
-          if (pRatings[pIndex]=== -1) {
-            break;
-          }
+            if (pRatings[pIndex]=== -1) {
+              break;
+            }
+
           pSortedUsername.push(this.passengers[pIndex].username);// add the username at the index of the highest rating
-          pSortedAvgrating.push(this.passengers[pIndex].avgRating);
-          pSortedNumTrips.push(this.passengers[pIndex].numTrips);
+          pSortedAvgrating.push(this.passengers[pIndex].avgRating);// add the avg rating at the proper index
+          pSortedNumTrips.push(this.passengers[pIndex].numTrips);// add the num of trips at the proper index
           pRatings[pIndex]=-1;// current max now becomes -1
         }
+
+        //  // make a new sorted passenger DTO to display all the sorted driver information
           for (var x = 0; x < pSortedUsername.length; x++) {
             this.sPassengers.push(new SortedPassengerDto(pSortedAvgrating[x], pSortedUsername[x], pSortedNumTrips[x]));
 
           }
 
-
   },
+  
   methods: {
 
   }
