a1fcac7574623e2f78db49b318ddcef296cf7ba7
Paul Attara <paul.attara@mail.mcgill.ca>
2018-11-23 15:05:55 -0500

    back to OG localhost

--- config/index.js
+++ config/index.js
@@ -24,8 +24,8 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8087,
-    //host:  'localhost',//comment out 
-    host:  'sharefare-web.herokuapp.com', //remove comment
+    host:  'localhost',//comment out 
+    //host:  'sharefare-web.herokuapp.com', //remove comment
     backendHost: 'sharefare.herokuapp.com',
     backendPort: 80,
     autoOpenBrowser: true,
