242ff650c3dddd6b8174cb241d453011a6f639ed
Noam Suissa <noam.suissa01@gmail.com>
2018-11-24 20:36:53 -0500

    back to http

--- src/jsFiles/account.js
+++ src/jsFiles/account.js
@@ -3,8 +3,8 @@ import { LoginEvent } from '../jsFiles/LogInEvent.js'
 var config = require('../../config')
 
 
-var frontendUrl = 'https://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'https://' + config.dev.backendHost + ':' + config.dev.backendPort
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
 
 var AXIOS = axios.create({
